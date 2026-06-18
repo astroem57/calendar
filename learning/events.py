5eab56eed903414324a09cbc65bf0bfb63647f37
Emily Cavanough <emilyc@Emilys-MacBook-Air-324.local>
2026-06-18 13:41:40 +1000

    calendar based classes

---
+++ learning/events.py
@@ -0,0 +1,16 @@
+# This is the file in which the method of storing event data is described
+# Learn CLASSES, figure out how to use them here
+# when you want to define something but haven't used it yet use pass
+class Event:
+    def __init__(self, title, day, start_time, duration):
+        self.title = title
+        self.day = day
+        self.start_time = start_time
+        self.duration = duration
+    
+    def end_time(self):
+        return self.start_time + self.duration
+    
+
+event = Event("Meeting", "January 1st", 8, 4)
+print(event.start_time, "to", event.end_time())
--- learning/learning_classes.py
+++ learning/learning_classes.py
@@ -12,3 +12,33 @@ bob = Person("Bob", 168, 21)
 
 print(bob.age)
 print(bob.age_in_n_years(5))
+
+class Book:
+    def __init__(self, title, author, page):
+        self.title = title
+        self.author = author
+        self.page = page
+
+book = Book("The Picture of Dorian Gray", "Oscar Wilde", 201)
+print(book.title)
+print(book.author)
+print(book.page)
+
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+    
+    def deposit(self, amount):
+        self.balance+= amount
+    
+    def withdraw(self, amount):
+        self.balance-= amount
+
+account = BankAccount("Me", 100)
+account.deposit(50)
+account.withdraw(40)
+
+print(account.owner)
+print(account.balance)
