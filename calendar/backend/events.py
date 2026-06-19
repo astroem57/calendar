dda12d45e1eec5a83da2a777a9cf9ba9dccd87ed
Emily Cavanough <emilyc@Emilys-MacBook-Air-324.local>
2026-06-19 15:53:12 +1000

    comments

--- calendar/backend/events.py
+++ calendar/backend/events.py
@@ -1,19 +1,23 @@
 
+# make a class for events that includes key details
+# make it so that if the user only knows end time or duration, the calendar can calculate both
+#make it so that the user has to input an end time or duration
 class Event:
     def __init__(self, title, date, description, start_time, end_time = None, duration = None,):
         self.title = title
         self.date = date
         self.start_time = start_time
         self.description = description
-
+#if the duration does not equal none then find end time but adding duration and start time
         if duration is not None:
             self.duration = duration
             self.end_time = duration + start_time
-        
+#if end_time is not none find the duration by taking start time from end time
         elif end_time is not None:
             self.end_time = end_time
             self.duration = duration
             self.duration = end_time - start_time
+#test
 event = Event("meeting", 1, "blah", 12, 13)
 
 print(event.duration)
--- learning/classes_with_events.py
+++ learning/classes_with_events.py
@@ -1,6 +1,7 @@
 # This is the file in which the method of storing event data is described
 # Learn CLASSES, figure out how to use them here
 # when you want to define something but haven't used it yet use pass
+
 class Event:
     def __init__(self, title, day, start_time, duration):
         self.title = title
