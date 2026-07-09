5e2077f930b5926dc0a3e31941c59a26a33a0034
Emily Cavanough <emilyc@Emilys-MacBook-Air-324.local>
2026-07-09 15:51:44 +1000

    made code to create folders and save the file

--- calendar/backend/events.py
+++ calendar/backend/events.py
@@ -1,7 +1,7 @@
 # make a class for events that includes key details
 # make it so that if the user only knows end time or duration, the calendar can calculate both
 #make it so that the user has to input an end time or duration
-
+import os
 class Event:
     def __init__(self, title, year, month, day, hour = None, minute = None, duration = None, description = None):
         self.title = title
@@ -17,11 +17,30 @@ class Event:
         print(self.title, self.year, self.month, self.day, self.hour, self.minute, self.duration, self.description)
     #fix after work
     def combine(self):
-        combined = f"{.title}<|> {.year}<|> {.month}<|> {.day}<|> {event.hour}<|> {event.minute}<|> {event.duration}<|> {event.description}"
+        combined = f"{self.title}<|> {self.year}<|> {self.month}<|> {self.day}<|> {self.hour}<|> {self.minute}<|> {self.duration}<|> {self.description}"
         return combined
-    
+    def foldername(self):
+        contents = f"data/{self.year},{self.month},{self.day}"
+        return contents
+    def filename(self):
+        return self.title
+    def path(self):
+        foldername = self.foldername()
+        filename = self.filename()
+        join = f"{foldername}/{filename}"
+        return join
+    def makefolder(self):
+        foldername = self.foldername()
+        if not os.path.exists(foldername):
+            os.makedirs(foldername)
+    def save(self):
+        self.makefolder()
+        filename = self.path()
+        combo = self.combine()
+        with open(filename, "w") as file:
+            file.write(combo)
 
-    
+        
 
 
 def unstringify_event(combined):
@@ -32,7 +51,13 @@ def unstringify_event(combined):
     day = int(parts[3])
     hour = int(parts[4])
     minute = int(parts[5])
-    duration = parts[6]
+    duration = int(parts[6])
     description = parts[7]
     event = Event(title, year, month, day, hour, minute, duration, description)
     return event
+
+eventss = Event("blah", 2007, 9, 10)
+
+eventss.save()
+
+#
